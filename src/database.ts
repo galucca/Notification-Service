a85493206b6579286ad800eaf81c986603f2bf42
Rodolfo Marques Ribeiro <rodolfomarqribeiro@gmail.com>
2025-05-07 23:46:56 -0300

    chore: add code review comments

--- src/database.ts
+++ src/database.ts
@@ -8,8 +8,9 @@ export async function connectDatabase() {
         await mongoose.connect(process.env.MONGO_URL!);
         console.log('✅ MongoDB conectado!');
     } catch (error) {
+        // seria interessante usar logger com diferentes níveis aqui e dar o throw ou como comentaste abaixo, um process.exit(1).
         console.error('❌ Erro ao conectar no MongoDB:', error);
-        console.warn('⚠️ Atenção: Continuando execução SEM banco de dados.');
+        console.warn('⚠️ Atenção: Continuando execução SEM banco de dados.')
         // Não usamos process.exit(1), assim o servidor continua rodando
     }
 }
